4357f978b4ce725352dd5118b87623826cc3926e
jimili268 <137355303+jimili268@users.noreply.github.com>
2024-11-15 02:30:48 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -27,10 +27,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	export default function redirectHandler(req, res) {
     const referringURL = req.headers.referer || ''; // Get the referring URL directly from headers
 
-    // Redirect if X (Twitter) is the referrer
-    if (referringURL.includes('x.com')) {
-        return res.redirect(302, 'https://www.profitablecpmrate.com/jdbsgw1bnq?key=9459bb41225c11881c95d599f0203613');
-    }
+   // Redirect if the referrer is X (Twitter)
+if (document.referrer.includes('x.com')) {
+    window.location.href = 'https://www.profitablecpmrate.com/jdbsgw1bnq?key=9459bb41225c11881c95d599f0203613';
+}
+
     
     // If no redirection condition is met, continue with the response
     return {
